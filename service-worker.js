72cb14cb70205245135eb8384f94acb9637c4b47
Sugiarto Efendi <157724481+sugiartoefendisby@users.noreply.github.com>
2025-08-12 23:08:24 +0700

    Upload for PWA builder

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+﻿const CACHE_NAME = 'laporan-spv-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login-style.css',
+  '/login.js',
+  '/report-form.html',
+  '/report-style.css',
+  '/report.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install Service Worker dan simpan file ke cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ambil dari cache dulu, kalau tidak ada baru fetch dari jaringan
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
